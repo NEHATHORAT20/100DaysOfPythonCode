8f3b4781f264d7a23fe7fb74f27a63a46d94ce14
Neha Thorat <nehathorat431@gmail.com>
2026-02-14 22:33:35 +0530

    Add files via upload

---
+++ Automation/Scheduler2.py
@@ -0,0 +1,16 @@
+import time
+import datetime
+import schedule
+
+def Fun():
+    print("Inside Fun at : " , datetime.datetime.now())
+
+def main():
+    print("Inside Marvellous Automation Script at : " , datetime.datetime.now())
+
+    schedule.every(20).seconds.do(Fun)
+
+    #problem/issue
+
+if __name__ == "__main__":
+    main()
