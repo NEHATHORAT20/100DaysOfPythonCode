14074c031170f6e0a9b4cb883d93606b721cb9d6
Neha Thorat <nehathorat431@gmail.com>
2026-02-25 00:25:34 +0530

    Add files via upload

---
+++ Automation/ProcessLoggerLogFile2.py
@@ -0,0 +1,64 @@
+import psutil
+import sys
+import os
+
+def CreateLog(FolderName):
+    Ret = False
+
+    Ret = os.path.exists(FolderName)
+
+    if(Ret == True):
+        Ret = os.path.isdir(FolderName)
+        if(Ret == False):
+            print("Unable to create folder")
+            return
+    else:
+        os.mkdir(FolderName)
+        print("Directory for log Files get created successfully")
+
+def main():
+    Border = "-"*50
+
+    print(Border)
+    print("-----Marvellous Platform Surveillance System------")
+    print(Border)
+
+    if (len(sys.argv) == 2):
+        if(sys.argv[1] == "--h" or sys.argv[1] == "--H"):
+            print("This script is used to :")
+            print("1: Create automatic logs")
+            print("2: Executes periodically")
+            print("3: Sends mail with log")
+            print("4: Store information about processes")
+            print("5: Store information about CPU")
+            print("6: Store information about RAM usage")
+            print("7: Store information about secondary storage")
+
+        elif(sys.argv[1] == "--u" or sys.argv[1] == "--U"):
+            print("Use the automation script as")
+            print("ScriptName.py TimeInterval DirectoryName")
+            print("Time Interval: The time in minutes for periodic scheduling")
+            print("Directory Name: Name of Directory to create auto logs")
+
+        else:
+            print("Unable to proceed as there is no such directory")
+            print("Please use --u or --h to get more details")
+
+    #python Demo.py 5 MarvellousProcess
+    elif(len(sys.argv) == 3):
+        print("Inside Projects Logic")
+        print("Time Interval : " , sys.argv[1])
+        print("Directory Name : " , sys.argv[2])
+        CreateLog(sys.argv[2])
+
+    else:
+        print("Invalid number of command line arguments")
+        print("Unable to proceed as there is no such directory")
+        print("Please use --u or --h to get more details")
+
+    print(Border)
+    print("----------Thank you for using our script----------")
+    print(Border)
+
+if __name__ == "__main__":
+    main()
---
+++ Automation/SystemSurveillance1.py
@@ -0,0 +1,137 @@
+import psutil
+import sys
+import os
+import time
+import schedule
+
+def CreateLog(FolderName):
+
+    Border = "-"*50
+
+    Ret = False
+
+    Ret = os.path.exists(FolderName)
+
+    if(Ret == True):
+        Ret = os.path.isdir(FolderName)
+        if(Ret == False):
+            print("Unable to create folder")
+            return
+    else:
+        os.mkdir(FolderName)
+        print("Directory for log Files get created successfully")
+
+    timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
+    print(timestamp)
+    FileName = os.path.join(FolderName , "Marvellous_%s.log" %timestamp)
+    print(FileName)
+
+    fobj = open(FileName , "w")
+
+    fobj.write(Border+"\n")
+    fobj.write("Marvellous Platform Surveillance System\n")
+    fobj.write("Log created at : " +time.ctime()+"\n")
+    fobj.write(Border+"\n\n")
+
+    fobj.write("-----------------System Report-----------------\n")
+
+    #print("CPU usage : " , psutil.cpu_percent())
+    fobj.write("CPU usage : %s  %%\n" %psutil.cpu_percent())
+    fobj.write(Border+"\n")
+
+    mem = psutil.virtual_memory()
+    #print("Ram usage : " , mem.percent)
+    fobj.write("RAM usage : %s  %%\n" %mem.percent)
+    fobj.write(Border+"\n")
+
+    fobj.write("\nDisk usage Report\n")
+    fobj.write(Border+"\n")
+
+    for part in psutil.disk_partitions():
+        try:
+            usage = psutil.disk_usage(part.mountpoint)
+            #print(f"{part.mountpoint} used {usage.percent}%%")
+            fobj.write("%s -> %s %% used\n" %(part.mountpoint , usage.percent))
+        except:
+            pass
+
+    fobj.write(Border+"\n")
+
+    net = psutil.net_io_counters()
+    fobj.write("\nNetwrok Usage Report\n")
+    fobj.write("Sent : %.2f MB\n" %(net.bytes_sent / (1024 * 1024)))
+    fobj.write("Receive : %.2f MB\n" %(net.bytes_recv / (1024 * 1024)))
+
+    #Process Log
+    fobj.write(Border+"\n")
+    fobj.write("------------------End of Log file-----------------\n")
+    fobj.write(Border+"\n")
+
+def ProcessScan():
+    print("Process Scan Report")
+
+    for proc in psutil.process_iter(attrs=["pid" , "name" , "status"]):
+        info = proc.info
+        print(info["pid"] , info["name"] , info["status"])
+
+def main():
+    ProcessScan()
+    return
+    
+    Border = "-"*50
+
+    print(Border)
+    print("-----Marvellous Platform Surveillance System------")
+    print(Border)
+
+    if (len(sys.argv) == 2):
+        if(sys.argv[1] == "--h" or sys.argv[1] == "--H"):
+            print("This script is used to :")
+            print("1: Create automatic logs")
+            print("2: Executes periodically")
+            print("3: Sends mail with log")
+            print("4: Store information about processes")
+            print("5: Store information about CPU")
+            print("6: Store information about RAM usage")
+            print("7: Store information about secondary storage")
+
+        elif(sys.argv[1] == "--u" or sys.argv[1] == "--U"):
+            print("Use the automation script as")
+            print("ScriptName.py TimeInterval DirectoryName")
+            print("Time Interval: The time in minutes for periodic scheduling")
+            print("Directory Name: Name of Directory to create auto logs")
+
+        else:
+            print("Unable to proceed as there is no such directory")
+            print("Please use --u or --h to get more details")
+
+    #python Demo.py 5 MarvellousProcess
+    elif(len(sys.argv) == 3):
+        print("Inside Projects Logic")
+        print("Time Interval : " , sys.argv[1])
+        print("Directory Name : " , sys.argv[2])
+        
+        #Apply the scheduler
+        schedule.every(int(sys.argv[1])).minutes.do(CreateLog, sys.argv[2])
+
+        print("Platform Surveillance System started successfully")
+        print("Directory created with name : " , sys.argv[2])
+        print("Time interval in minutes : " , sys.argv[1])
+        print("Press Ctrl + C to stop execution")
+
+        #Wait till abort
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    else:
+        print("Invalid number of command line arguments")
+        print("Unable to proceed as there is no such directory")
+        print("Please use --u or --h to get more details")
+
+    print(Border)
+    print("----------Thank you for using our script----------")
+    print(Border)
+
+if __name__ == "__main__":
+    main()
